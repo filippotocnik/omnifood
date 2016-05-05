5cd437228f81af4448c8a78e98e1428f9d7adcdc
Filip <filip.potocnik@gmail.com>
2016-05-05 15:27:46 +0200

    first commit

---
+++ resourses/js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function () {
+
+    $('.js--section-features').waypoints(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }, {
+            offset: '60px;'
+        }
+    });
+
+});
+
